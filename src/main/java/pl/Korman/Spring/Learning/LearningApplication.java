540f561bffdc6a698e58d02132a001a1cb523755
Rafał Korman <rafalkor5@gmail.com>
2022-03-31 15:35:06 +0200

    Start Repo

---
+++ src/main/java/pl/Korman/Spring/Learning/LearningApplication.java
@@ -0,0 +1,15 @@
+package pl.Korman.Spring.Learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LearningApplication.class, args);
+
+	}
+
+}
