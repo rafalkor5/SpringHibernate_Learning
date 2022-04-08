2820556f69aa27e61cb00ba06ea43c3100f384db
Rafał Korman <rafalkor5@gmail.com>
2022-04-08 15:58:10 +0200

    Add own ConfiguratorProperties and infoController

---
+++ src/main/java/pl/Korman/Spring/Learning/controller/InfoController.java
@@ -0,0 +1,26 @@
+package pl.Korman.Spring.Learning.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.Korman.Spring.Learning.model.TaskConfigurationProperties;
+
+//Tu przekazujemy dane do restcontrollera
+@AllArgsConstructor
+@RestController
+class InfoController {
+
+    private DataSourceProperties dataSource;
+    private TaskConfigurationProperties allowMulti;
+
+    @GetMapping("/info/url")
+    String url(){
+    return dataSource.getUrl();
+    }
+
+    @GetMapping("/info/prop")
+    boolean myProp(){
+    return allowMulti.getTemplate().isAllowMultipleTasks();
+    }
+}
---
+++ src/main/java/pl/Korman/Spring/Learning/model/TaskConfigurationProperties.java
@@ -0,0 +1,23 @@
+package pl.Korman.Spring.Learning.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+// Ta klasa automatycznie pobiera dane z application.yaml
+@Configuration
+@ConfigurationProperties("task")
+public class TaskConfigurationProperties {
+
+    @Getter
+    @Setter
+    private Template template;
+
+    @Getter
+    @Setter
+    public static class Template {
+        private boolean allowMultipleTasks;
+
+    }
+}
