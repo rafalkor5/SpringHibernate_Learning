79a21453f71540ff065ffcf1b9ec224c0086ebaf
Rafał Korman <rafalkor5@gmail.com>
2022-04-12 22:47:34 +0200

    Add example @Querry @Override hibernate method

--- src/main/java/pl/Korman/Spring/Learning/model/SqlTaskRepository.java
+++ src/main/java/pl/Korman/Spring/Learning/model/SqlTaskRepository.java
@@ -1,10 +1,14 @@
 package pl.Korman.Spring.Learning.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 //To musi być aby działało taskrepository
 @Repository
 interface SqlTaskRepository extends TaskRepository, JpaRepository<Task,Integer> {
-
+    @Query(nativeQuery = true, value="select  count(*) > 0 from tasks where id=:id")
+    @Override
+    boolean existsById(@Param("id") Integer id);
 }
