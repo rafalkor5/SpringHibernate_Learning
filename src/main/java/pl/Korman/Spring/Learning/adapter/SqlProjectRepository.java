dae3af349cd1ec4c868ef42c9f9f359a3cb3627d
Rafał Korman <rafalkor5@gmail.com>
2022-04-27 17:12:08 +0200

    Update project repo

--- src/main/java/pl/Korman/Spring/Learning/adapter/SqlProjectRepository.java
+++ src/main/java/pl/Korman/Spring/Learning/adapter/SqlProjectRepository.java
@@ -1,11 +1,19 @@
 package pl.Korman.Spring.Learning.adapter;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.Korman.Spring.Learning.model.project.Project;
 import pl.Korman.Spring.Learning.model.project.ProjectRepository;
+import pl.Korman.Spring.Learning.model.taskgroup.TaskGroup;
+
+import java.util.List;
 
 @Repository
 public interface SqlProjectRepository extends ProjectRepository, JpaRepository<Project, Integer> {
+
+    @Override
+    @Query("select distinct p from Project p join fetch p.projectsteps ")
+    List<Project> findAll();
 
 }
